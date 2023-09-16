21ef10138996e527b76f55e30185c18d1308af21
creditFool <bichlasulbulqiah@gmail.com>
2023-09-17 03:59:48 +0700

    add db query from sqlc

---
+++ db/query/entry.sql
@@ -0,0 +1,21 @@
+-- name: CreateEntry :one
+INSERT INTO entry (
+  account,
+  amount
+) VALUES (
+  $1, $2
+) RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entry
+WHERE id = $1 LIMIT 1;
+
+-- name: ListEntry :many
+SELECT * FROM entry
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: DeleteEntry :exec
+DELETE FROM entry
+WHERE id = $1;
